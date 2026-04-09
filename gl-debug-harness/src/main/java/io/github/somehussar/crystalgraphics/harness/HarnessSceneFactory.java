1cb16872a7f26b9bb69a9030b4de42a18bb9c461
bigguy345 <monkeybar3200@gmail.com>
2026-04-09 17:35:22 +0200

    correctly link gl-debug-harness as a submodule

--- gl-debug-harness/src/main/java/io/github/somehussar/crystalgraphics/harness/HarnessSceneFactory.java
+++
@@ -1,9 +0,0 @@
-package io.github.somehussar.crystalgraphics.harness;
-
-/**
- * Factory for creating {@link HarnessSceneLifecycle} instances.
- * Used by the registry to defer scene instantiation until needed.
- */
-public interface HarnessSceneFactory {
-    HarnessSceneLifecycle create();
-}
